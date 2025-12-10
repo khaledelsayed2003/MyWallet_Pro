4695d1381db8b996c1418b1af95dd13d9168cb26
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-10 22:04:22 +0300

    feat(models): implement Transaction model using SQLAlchemy with date default and custom __repr__

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # local SQLite file
+db = SQLAlchemy(app)
 
+
+# --- MODELS ---
+class Transaction(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(10), nullable=False)  # "income" or "expense"
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    note = db.Column(db.String(255))
+    
+    
+    def __repr__(self):
+        return f'<Transaction {self.id}>'   
+    
+    
+    
+    
+    
 
 @app.route('/')
 def home():
