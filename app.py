adbc15d22660f074c959d9ef37d3484d2dd218b0
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-12-20 13:54:57 +0300

    feat: add user model with password hashing

--- app.py
+++ app.py
@@ -21,6 +21,22 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 
+# --- MODELS ---
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password: str):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password: str) -> bool:
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+
+
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -48,5 +64,7 @@ def add_transaction():
     return render_template("add_transaction.html")
 
 
-if __name__  == "__main__":
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  #only creates if doesn't exist
     app.run(debug=True)
